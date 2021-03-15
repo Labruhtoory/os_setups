47926f0a7784d218dedd19f333acb322a16c2c9a
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-03-15 09:40:05 -0900

    Update ctf_os_init.sh

--- ctf_os_init.sh
+++ ctf_os_init.sh
@@ -43,10 +43,12 @@ if [$(full) == "n"]; then
   read -p 'Post?> ' post
   read -p 'Anon?> ' anon
   if [$(net) == "n"]; then
+  echo 'No net...'
   else
   sudo apt install -fy nmap masscan wireshark aircrack-ng wifite pixiewps bettercap clusterssh netdiscover reaver rtpflood
   fi
   if [$(recon) == "n"]; then
+  echo 'No recon...'
   else
   sudo apt install -fy recon-ng aircrack-ng rtlsdr-scanner gqrx-sdr osrframework legion dnsrecon thc-ipv6 uniscan ffuf
 								
@@ -69,6 +71,7 @@ if [$(full) == "n"]; then
   git clone https://github.com/s0md3v/Photon.git
   fi
   if [$(enum) == "n"]; then
+  echo 'No enum...'
   else
   sudo apt install -fy nikto voiphopper zaproxy burpsuite enum4linux dmitry dnstracer dirb dirbuster gobuster wpscan siparmyknife theharvester
 
@@ -92,6 +95,7 @@ if [$(full) == "n"]; then
   git clone https://github.com/maurosoria/dirsearch.git
   fi
   if [$(revfor) == "n"]; then
+  echo 'No revfor...'
   sudo apt install -fy exiftool binwalk radare2 gdb audacity arduino
 
   #hopper
@@ -112,6 +116,7 @@ if [$(full) == "n"]; then
   else
   fi
   if [$(xploit) == "n"]; then
+  echo 'No xploit...'
   else
   sudo apt install -fy exploitdb set sqlmap sqlninja websploit powersploit veil-evasion beef-xss
 
@@ -130,6 +135,7 @@ if [$(full) == "n"]; then
   git clone https://github.com/EmpireProject/Empire.git
   fi
   if [$(post) == "n"]; then
+  echo 'No post...'
   else
   sudo apt install -fy neo4j wordlists backdoor-factory crunch hash-identifier john johnny rainbowcrack hashcat
 		
@@ -143,13 +149,13 @@ if [$(full) == "n"]; then
   mkdir /usr/share/neo4j/run
   #defaul creds - neo4j:neo4j - need to change
   fi
-  read -p 'Anon?> ' anon
   if [$(anon) == "n"]; then
+  echo 'No anon...'
   else
   sudo apt install -fy openvpn tor torbrowser-launcher
   fi
-  read -p 'Custom?> ' cus
   if [$(cus) == "n"]; then
+  echo 'No custom...'
   else
   sudo apt install -fy gnome-tweaks
 
@@ -171,94 +177,94 @@ else
   sudo apt install -fy wireshark kali-menu python3 python3-pip sqlitebrowser golang terminator openvpn gnome-tweaks exiftool nasm binwalk default-jdk radare2 gdb gqrx-sdr clusterssh audacity neo4j tor torbrowser-launcher nmap masscan exploitdb armitage set nikto osrframework recon-ng netdiscover legion voiphopper zaproxy enum4linux dmitry dnsrecon dnstracer theharvester thc-ipv6 reaver aircrack-ng rtlsdr-scanner gqrx-sdr wifite pixiewps burpsuite dirb dirbuster gobuster wpscan wordlists sqlmap sqlninja uniscan websploit ffuf siparmyknife powersploit backdoor-factory veil-evasion bettercap beef-xss rtpflood crunch hash-identifier john johnny rainbowcrack hashcat arduino
   
   #unicorn
-    git clone https://github.com/trustedsec/unicorn.git
+  git clone https://github.com/trustedsec/unicorn.git
 
-    #urh
-    git clone https://github.com/jopohl/urh.git
+  #urh
+  git clone https://github.com/jopohl/urh.git
     
-    #linpeas
-    git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
-    mkdir peas
-    mv privilege-escalation-awesome-scripts-suite/* peas/
-    rm -rf privilege-escalation-awesome-scripts-suite/
-    cp peas/linPEAS/linpeas.sh /opt/server/
-    cp peas/winPEAS/winPEASbat/winPEAS.bat /opt/server/
+  #linpeas
+  git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
+  mkdir peas
+  mv privilege-escalation-awesome-scripts-suite/* peas/
+  rm -rf privilege-escalation-awesome-scripts-suite/
+  cp peas/linPEAS/linpeas.sh /opt/server/
+  cp peas/winPEAS/winPEASbat/winPEAS.bat /opt/server/
     
-    #PSEmpire
-    git clone https://github.com/EmpireProject/Empire.git
+  #PSEmpire
+  git clone https://github.com/EmpireProject/Empire.git
 
-    #ILSpy decompiler
-    git clone https://github.com/icsharpcode/ILSpy.git
+  #ILSpy decompiler
+  git clone https://github.com/icsharpcode/ILSpy.git
 
-    #poor mans pentest
-    gti clone https://github.com/JohnHammond/poor-mans-pentest.git
-    mkdir pmp
-    mv poor-mans-pentest/* pmp/
-    rm -rf poor-mans-pentest
+  #poor mans pentest
+  git clone https://github.com/JohnHammond/poor-mans-pentest.git
+  mkdir pmp
+  mv poor-mans-pentest/* pmp/
+  rm -rf poor-mans-pentest
 
-    #dirsearch
-    git clone https://github.com/maurosoria/dirsearch.git
+  #dirsearch
+  git clone https://github.com/maurosoria/dirsearch.git
 
-    #phoninfoga
-    curl -sSL https://raw.githubusercontent.com/sundowndev/PhoneInfoga/master/support/scripts/install | bash
-    mv ./phoneinfoga /usr/bin/phoneinfoga
+  #phoninfoga
+  curl -sSL https://raw.githubusercontent.com/sundowndev/PhoneInfoga/master/support/scripts/install | bash
+  mv ./phoneinfoga /usr/bin/phoneinfoga
 
-    #photon
-    git clone https://github.com/s0md3v/Photon.git
+  #photon
+  git clone https://github.com/s0md3v/Photon.git
 
-    #hopper
-    wget https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-v4-4.7.1-Linux.deb
-    dpkg -i Hopper-v4-4.7.1-Linux.deb
-    rm -rf Hopper-v4-4.7.1-Linux.deb
+  #hopper
+  wget https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-v4-4.7.1-Linux.deb
+  dpkg -i Hopper-v4-4.7.1-Linux.deb
+  rm -rf Hopper-v4-4.7.1-Linux.deb
     
-    #subl
-    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-    sudo apt-get install apt-transport-https
-    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-    sudo apt-get update
-    sudo apt-get install sublime-text
+  #subl
+  wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+  sudo apt-get install apt-transport-https
+  echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+  sudo apt-get update
+  sudo apt-get install sublime-text
     
-    #rphp
-    git clone https://github.com/pentestmonkey/php-reverse-shell.git 
-    mv php-reverse-shell/php-reverse-shell.php /opt/server/rphp.php
-    rm -rf php-reverse-shell
+  #rphp
+  git clone https://github.com/pentestmonkey/php-reverse-shell.git 
+  mv php-reverse-shell/php-reverse-shell.php /opt/server/rphp.php
+  rm -rf php-reverse-shell
     
-    #pspy
-    wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32
-    wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64
-    mv pspy32 /opt/server/
-    mv pspy64 /opt/server/
+  #pspy
+  wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32
+  wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64
+  mv pspy32 /opt/server/
+  mv pspy64 /opt/server/
     
-    #linenum
-    wget https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh
-    mv LinEnum.sh /opt/server/linenum.sh
+  #linenum
+  wget https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh
+  mv LinEnum.sh /opt/server/linenum.sh
 
-    #kerbrute
-    wget https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_linux_amd64
-    mv kerbrute_linux_amd64 /usr/bin/kerbrute
+  #kerbrute
+  wget https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_linux_amd64
+  mv kerbrute_linux_amd64 /usr/bin/kerbrute
 
-    #rustscan
-    wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
-    dpkg -i rustscan_2.0.1_amd64.deb
-    rm -rf /opt/rustscan_2.0.1_amd64.deb
+  #rustscan
+  wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
+  dpkg -i rustscan_2.0.1_amd64.deb
+  rm -rf /opt/rustscan_2.0.1_amd64.deb
     
-    #bloodhound
-    wget https://github.com/BloodHoundAD/BloodHound/releases/download/4.0.2/BloodHound-linux-x64.zip
-    unzip BloodHound-linux-x64.zip
-    rm -rf BloodHound-linux-x64.zip
-    mkdir bloodhound
-    mv BloodHound-linux-x64/* bloodhound/
-    mkdir /usr/share/neo4j/logs
-    mkdir /usr/share/neo4j/run
-    #defaul creds - neo4j:neo4j - need to change
+  #bloodhound
+  wget https://github.com/BloodHoundAD/BloodHound/releases/download/4.0.2/BloodHound-linux-x64.zip
+  unzip BloodHound-linux-x64.zip
+  rm -rf BloodHound-linux-x64.zip
+  mkdir bloodhound
+  mv BloodHound-linux-x64/* bloodhound/
+  mkdir /usr/share/neo4j/logs
+  mkdir /usr/share/neo4j/run
+  #defaul creds - neo4j:neo4j - need to change
 
-    #ghidra
-    wget https://ghidra-sre.org/ghidra_9.2.2_PUBLIC_20201229.zip
-    unzip ghidra_9.2.2_PUBLIC_20201229.zip
-    rm -rf ghidra_9.2.2_PUBLIC_20201229.zip
-    mkdir ghidra
-    mv ghidra_9.2.2_PUBLIC_20201229/* ghidra/
-    rm -rf ghidra_9.2.2_PUBLIC_20201229/
+  #ghidra
+  wget https://ghidra-sre.org/ghidra_9.2.2_PUBLIC_20201229.zip
+  unzip ghidra_9.2.2_PUBLIC_20201229.zip
+  rm -rf ghidra_9.2.2_PUBLIC_20201229.zip
+  mkdir ghidra
+  mv ghidra_9.2.2_PUBLIC_20201229/* ghidra/
+  rm -rf ghidra_9.2.2_PUBLIC_20201229/
 fi
 
 #wireless drivers
