f1d63fa86521e7d982e0158985ce1d8265c6ff39
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-03-15 09:06:00 -0900

    Update ctf_os_init.sh

--- ctf_os_init.sh
+++ ctf_os_init.sh
@@ -7,6 +7,7 @@ apt-key adv --keyserver pool.sks-keyservers.net --recv-keys ED444FF07D8D0BF6
 echo '# Kali linux repositories | Added by Katoolin' >> /etc/apt/sources.list
 echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
 apt-get update
+sudo apt install -fy kali-menu
 
 
 echo 'moving to /opt'
