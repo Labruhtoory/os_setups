29d6148e6f5292da215bafc01692039b9aac5f29
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-03-23 17:56:00 -0900

    Update deb_os.sh

--- deb_os.sh
+++ deb_os.sh
@@ -199,6 +199,9 @@ then
   mkdir pmp
   mv poor-mans-pentest/* pmp/
   rm -rf poor-mans-pentest
+  
+  #cherrytree
+  sudo apt install -y cherrytree
   fi
 else
   echo "OK, installing all packages"
@@ -315,15 +318,6 @@ cd rtl8812au/
 make && make install
 cd /opt/
     
-#root aliases
-echo "alias up='python3 -m http.server 8000'" >> ~/.bashrc
-echo "alias room='bash /opt/wagames_os/notes/room-box_notes_start.sh'" >> ~/.bashrc
-echo "alias dirsearch='python3 /opt/dirsearch/dirsearch.py'" >> ~/.bashrc
-echo "alias ghidra='bash /opt/ghidra/ghidraRun'" >> ~/.bashrc
-echo "alias photon='python3 /opt/Photon/photon.py'" >> ~/.bashrc
-echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> ~/.bashrc
-echo "alias screenres='bash /opt/screenres.sh'" >> ~/.bashrc
-
 #user aliases
 echo "alias up='python3 -m http.server 8000'" >> ~/.bashrc
 echo "alias room='bash /opt/wagames_os/notes/room-box_notes_start.sh'" >> ~/.bashrc
@@ -332,6 +326,19 @@ echo "alias ghidra='bash /opt/ghidra/ghidraRun'" >> ~/.bashrc
 echo "alias photon='python3 /opt/Photon/photon.py'" >> ~/.bashrc
 echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> ~/.bashrc
 echo "alias screenres='bash /opt/screenres.sh'" >> ~/.bashrc
+echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> ~/.bashrc
+echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> ~/.bashrc
+
+#root aliases
+echo "alias up='python3 -m http.server 8000'" >> /root/.bashrc
+echo "alias room='bash /opt/wagames_os/notes/room-box_notes_start.sh'" >> /root/.bashrc
+echo "alias dirsearch='python3 /opt/dirsearch/dirsearch.py'" >> /root/.bashrc
+echo "alias ghidra='bash /opt/ghidra/ghidraRun'" >> /root/.bashrc
+echo "alias photon='python3 /opt/Photon/photon.py'" >> /root/.bashrc
+echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /root/.bashrc
+echo "alias screenres='bash /opt/screenres.sh'" >> /root/.bashrc
+echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> ~/.bashrc
+echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> ~/.bashrc
 
 chmod -R 777 /opt/
 reboot now
