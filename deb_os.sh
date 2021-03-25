1bd4fedf22917e18db90f737994dcd12729cb183
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-03-24 15:49:03 -0900

    Update deb_os.sh

--- deb_os.sh
+++ deb_os.sh
@@ -296,7 +296,7 @@ else
   sudo apt install software-properties-common apt-transport-https wget
   wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-  sudo apt install code
+  sudo apt install -fy code
     
   #rphp
   git clone https://github.com/pentestmonkey/php-reverse-shell.git 
