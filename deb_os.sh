6187e85acf5e945744ccee2ab5c4063d59f7437f
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-03-24 15:46:23 -0900

    Update deb_os.sh

--- deb_os.sh
+++ deb_os.sh
@@ -288,7 +288,7 @@ else
   sudo apt-get install apt-transport-https
   echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
   sudo apt-get update
-  sudo apt-get install sublime-
+  sudo apt-get install sublime-text
   
   
   #vscode
