1c03525d5fff10f60840ef8ab1997632a07b3e6c
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-03-25 02:33:31 +0000

    Update add_aliases.sh

--- add_aliases.sh
+++ add_aliases.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-#user aliases
-echo "alias up='python3 -m http.server 8000'" >> ~/.bashrc
-echo "alias room='bash /opt/wagames_os/notes/room-box_notes_start.sh'" >> ~/.bashrc
-echo "alias dirsearch='python3 /opt/dirsearch/dirsearch.py'" >> ~/.bashrc
-echo "alias ghidra='bash /opt/ghidra/ghidraRun'" >> ~/.bashrc
-echo "alias photon='python3 /opt/Photon/photon.py'" >> ~/.bashrc
-echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> ~/.bashrc
-echo "alias screenres='bash /opt/screenres.sh'" >> ~/.bashrc
-echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> ~/.bashrc
-echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> ~/.bashrc
 
-sudo su
+read -p "Username 4 this box: " uboi
+
+#user aliases
+echo "alias up='python3 -m http.server 8000'" >> /home/$uboi/.bashrc
+echo "alias room='bash /opt/wagames_os/notes/room-box_notes_start.sh'" >> /home/$uboi/.bashrc
+echo "alias dirsearch='python3 /opt/dirsearch/dirsearch.py'" >> /home/$uboi/.bashrc
+echo "alias ghidra='bash /opt/ghidra/ghidraRun'" >> /home/$uboi/.bashrc
+echo "alias photon='python3 /opt/Photon/photon.py'" >> /home/$uboi/.bashrc
+echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /home/$uboi/.bashrc
+echo "alias screenres='bash /opt/screenres.sh'" >> /home/$uboi/.bashrc
+echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> /home/$uboi/.bashrc
+echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> /home/$uboi/.bashrc
+
+source /home/$uboi/.bashrc
 
 #root aliases
 echo "alias up='python3 -m http.server 8000'" >> /root/.bashrc
@@ -21,7 +24,9 @@ echo "alias ghidra='bash /opt/ghidra/ghidraRun'" >> /root/.bashrc
 echo "alias photon='python3 /opt/Photon/photon.py'" >> /root/.bashrc
 echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /root/.bashrc
 echo "alias screenres='bash /opt/screenres.sh'" >> /root/.bashrc
-echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> ~/.bashrc
-echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> ~/.bashrc
+echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> /root/.bashrc
+echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> /root/.bashrc
+
+source /root/.bashrc
 
 echo DONE!
