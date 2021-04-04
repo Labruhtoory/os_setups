1b65faadf40c348fef1da914f40e67c40087a8aa
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-03 22:04:10 -0900

    Update deb_os.sh

--- deb_os.sh
+++ deb_os.sh
@@ -113,7 +113,7 @@ then
   then
   echo 'No enum...'
   else
-  sudo apt install -fy nikto voiphopper zaproxy burpsuite enum4linux dmitry dnstracer dirb dirbuster gobuster wpscan siparmyknife theharvester
+  sudo apt install -fy nikto voiphopper zaproxy burpsuite enum4linux dsniff dmitry dnstracer dirb dirbuster gobuster wpscan siparmyknife theharvester
   sudo apt --fix-broken install
   
   #linenum
@@ -238,7 +238,7 @@ else
   #programming language support
   sudo python3 -m pip install --upgrade 
   
-  sudo apt install -fy wireshark python3 python3-pip sqlitebrowser golang terminator openvpn gnome-tweaks exiftool nasm binwalk default-jdk radare2 gdb gqrx-sdr clusterssh audacity bloodhound tor torbrowser-launcher nmap masscan exploitdb armitage set nikto osrframework recon-ng netdiscover legion voiphopper zaproxy enum4linux dmitry dnsrecon dnstracer theharvester thc-ipv6 reaver aircrack-ng rtlsdr-scanner gqrx-sdr wifite pixiewps burpsuite dirb dirbuster gobuster wpscan wordlists sqlmap sqlninja uniscan websploit ffuf siparmyknife powersploit backdoor-factory veil-evasion bettercap beef-xss rtpflood crunch hash-identifier john johnny rainbowcrack hashcat arduino
+  sudo apt install -fy wireshark python3 python3-pip sqlitebrowser golang terminator openvpn gnome-tweaks exiftool nasm binwalk default-jdk radare2 gdb gqrx-sdr clusterssh audacity bloodhound tor torbrowser-launcher nmap masscan exploitdb armitage set dsniff nikto osrframework recon-ng netdiscover legion voiphopper zaproxy enum4linux dmitry dnsrecon dnstracer theharvester thc-ipv6 reaver aircrack-ng rtlsdr-scanner gqrx-sdr wifite pixiewps burpsuite dirb dirbuster gobuster wpscan wordlists sqlmap sqlninja uniscan websploit ffuf siparmyknife powersploit backdoor-factory veil-evasion bettercap beef-xss rtpflood crunch hash-identifier john johnny rainbowcrack hashcat arduino
   sudo apt --fix-broken install
   
   
