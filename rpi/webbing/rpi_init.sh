d2fbcbb872fba52d4fdd9dd59a09fd46f4ba3783
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-04 21:17:54 -0900

    Update rpi_init.sh

--- rpi/webbing/rpi_init.sh
+++ rpi/webbing/rpi_init.sh
@@ -9,11 +9,6 @@ echo "" > /etc/apt/sources.list
 apt-key adv --keyserver pool.sks-keyservers.net --recv-keys ED444FF07D8D0BF6
 echo '# Kali linux repositories | Added by Katoolin' >> /etc/apt/sources.list
 echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
-apt-get update
-sudo apt install -fy python python3 python3-pip golang nasm default-jdk
-
-#programming language support
-sudo python3 -m pip install --upgrade pip
 
 echo 'moving to /opt'
 #init
@@ -23,56 +18,9 @@ mkdir /webtmp
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
-echo 'started decision process... deciding'
-<<Blockthis4now
-if [ $full == "n" ] 
-then
-  echo "OK, Please choose you packages (y/n)..."
-  read -p 'Cloud?> ' cloud
-  read -p 'Anon?> ' anon
-  read -p 'Hosting?> ' hosting
-  read -p 'Sec?> ' sec
-  if [ $(cloud) == 'n' ]
-  then
-  echo 'No Clouds...'
-  if [ $(anon) == 'n' ]
-  then
-  echo 'No Anon...'
-  else
-  sudo apt install -fy openvpn tor proxychains
-  fi
-  if [ $(hosting) == 'n' ]
-  then
-  echo 'No Hosting...'
-  else
-  sudo apt install nginx
-
-  fi
-  if [ $(sec) == 'n' ]
-  then
-  echo 'No sec...'
-  else
-  sudo apt install -fy nikto dmitry dnstracer dirb dirbuster gobuster exiftool binwalk radare2 gdb 
-  #dirsearch
-  git clone https://github.com/maurosoria/dirsearch.git
-  
-  #photon
-  git clone https://github.com/s0md3v/Photon.git
-  fi				
-
-else
-  echo "OK, installing all packages"
-  sudo apt install -fy python python3 python3-pip golang nasm default-jdk openvpn tor proxychains nginx nikto dmitry dnstracer dirb dirbuster gobuster exiftool binwalk radare2 gdb
-
-  #dirsearch
-  git clone https://github.com/maurosoria/dirsearch.git
-
-  #photon
-  git clone https://github.com/s0md3v/Photon.git
-fi
-Blockthis4now
-
 sudo apt install -fy python python3 python3-pip golang nasm default-jdk openvpn tor proxychains nginx nikto dmitry dnstracer dirb dirbuster gobuster exiftool binwalk radare2 gdb
+#programming language support
+sudo python3 -m pip install --upgrade pip
 
 #dirsearch
 git clone https://github.com/maurosoria/dirsearch.git
