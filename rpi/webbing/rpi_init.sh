5cd355bafff81b195f339cb9b4afc482181123e8
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-04 20:26:23 -0900

    Update rpi_init.sh

--- rpi/webbing/rpi_init.sh
+++ rpi/webbing/rpi_init.sh
@@ -20,8 +20,8 @@ echo 'moving to /opt'
 mkdir /webtmp
 
 #networking
-echo "nameserver 1.1.1.1" > /etv/resolv.conf
-echo "nameserver 1.0.0.1" >> /etv/resolv.conf
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
 if [ $(full) == 'n' ] 
 then
