e3f7e4a5fcf2efee924bbb3a79453a83887020c6
Labruhtooryboi <67720330+Labruhtooryboi@users.noreply.github.com>
2021-04-05 09:17:42 -0900

    changes

--- rpi/webbing/rpi_init.sh
+++ rpi/webbing/rpi_init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo 'keep in mind the default answer is yes...'
-read -p 'Full? y/n> ' full
-
 echo 'adding apt repo'
 #repo
 echo "" > /etc/apt/sources.list
@@ -11,6 +8,8 @@ echo '# Kali linux repositories | Added by Katoolin' >> /etc/apt/sources.list
 echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
 
 echo 'moving to /opt'
+cd /opt
+
 #init
 mkdir /webtmp
 
@@ -18,7 +17,7 @@ mkdir /webtmp
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
-sudo apt install -fy python python3 python3-pip golang openvpn tor proxychains nginx nikto dmitry dnstracer dirb dirbuster gobuster exiftool binwalk radare2 gdb
+sudo apt install -fy python python3 python3-pip golang openvpn tor proxychains nikto dmitry dnstracer dirb dirbuster gobuster exiftool binwalk radare2 gdb nginx
 #programming language support
 sudo python3 -m pip install --upgrade pip
 
