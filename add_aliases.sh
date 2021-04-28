ee0330a5f56bcd98c7e62ee37165c667dce6be44
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-27 22:30:17 -0400

    Update add_aliases.sh

--- add_aliases.sh
+++ add_aliases.sh
@@ -4,29 +4,29 @@
 read -p "Username 4 this box: " uboi
 
 #user aliases
-echo "alias up='python3 -m http.server 8000'" >> /home/$uboi/.bashrc
-echo "alias room='bash /opt/wagames_os/notes/room-box_notes_start.sh'" >> /home/$uboi/.bashrc
-echo "alias dirsearch='python3 /opt/dirsearch/dirsearch.py'" >> /home/$uboi/.bashrc
+echo "alias up='python3 -m http.server 8000'" >> /home/$uboi/.bash_aliases
+echo "alias room='bash /opt/notes/room-box_notes_start.sh'" >> /home/$uboi/.bash_aliases
+echo "alias dirsearch='python3 /opt/dirsearch/dirsearch.py'" >> /home/$uboi/.bash_aliases
 echo "alias ghidra='bash /opt/ghidra/ghidraRun'" >> /home/$uboi/.bashrc
 echo "alias photon='python3 /opt/Photon/photon.py'" >> /home/$uboi/.bashrc
-echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /home/$uboi/.bashrc
-echo "alias screenres='bash /opt/screenres.sh'" >> /home/$uboi/.bashrc
-echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> /home/$uboi/.bashrc
-echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> /home/$uboi/.bashrc
+echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /home/$uboi/.bash_aliases
+echo "alias screenres='bash /opt/screenres.sh'" >> /home/$uboi/.bash_aliases
+echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> /home/$uboi/.bash_aliases
+echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> /home/$uboi/.bash_aliases
 
-source /home/$uboi/.bashrc
+source /home/$uboi/.bash_aliases
 
 #root aliases
 echo "alias up='python3 -m http.server 8000'" >> /root/.bashrc
-echo "alias room='bash /opt/wagames_os/notes/room-box_notes_start.sh'" >> /root/.bashrc
-echo "alias dirsearch='python3 /opt/dirsearch/dirsearch.py'" >> /root/.bashrc
-echo "alias ghidra='bash /opt/ghidra/ghidraRun'" >> /root/.bashrc
-echo "alias photon='python3 /opt/Photon/photon.py'" >> /root/.bashrc
-echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /root/.bashrc
-echo "alias screenres='bash /opt/screenres.sh'" >> /root/.bashrc
-echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> /root/.bashrc
-echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> /root/.bashrc
+echo "alias room='bash /opt/wagames_os/notes/room-box_notes_start.sh'" >> /root/.bash_aliases
+echo "alias dirsearch='python3 /opt/dirsearch/dirsearch.py'" >> /root/.bash_aliases
+echo "alias ghidra='bash /opt/ghidra/ghidraRun'" >> /root/.bash_aliases
+echo "alias photon='python3 /opt/Photon/photon.py'" >> /root/.bash_aliases
+echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /root/.bash_aliases
+echo "alias screenres='bash /opt/screenres.sh'" >> /root/.bash_aliases
+echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> /root/.bash_aliases
+echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> /root/.bash_aliases
 
-source /root/.bashrc
+source /root/.bash_aliases
 
 echo DONE!
