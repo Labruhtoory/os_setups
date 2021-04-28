a3c1efa4389c4362cb7fc720d838bc5d8423d852
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-27 22:21:47 -0400

    Update room-box_notes_start.sh

--- notes/room-box_notes_start.sh
+++ notes/room-box_notes_start.sh
@@ -1,15 +1,13 @@
 
 #!/bin/bash
 
+subl reeadme.txt
 subl notes.txt
-subl readme.txt
-subl notes.txt
-subl readme.txt
-subl /opt/wargames_os/notes/report-template-1.0.txt
-subl /opt/wargames_os/notes/revshells.txt
-subl /opt/wargames_os/notes/revshellls2.0.txt
-subl /opt/wargames_os/notes/koth-specific/metwis-lin.txt
-subl /opt/wargames_os/notes/koth-specific/metwis-win.txt
+subl /opt/notes/report-template-1.0.txt
+subl /opt/notes/revshells.txt
+subl /opt/notes/revshellls2.0.txt
+subl /opt/notes/koth-specific/metwis-lin.txt
+subl /opt/notes/koth-specific/metwis-win.txt
 mkdir recon
 mkdir enum
 mkdir exploitation
