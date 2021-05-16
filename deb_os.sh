04bf616aefd0f4ae3f9e88f51debc0d18d9b7fd1
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-05-16 23:30:14 +0000

    Update deb_os.sh

--- deb_os.sh
+++ deb_os.sh
@@ -244,7 +244,8 @@ else
   #initial stuff
   sudo apt install -fy python3 python3-pip sqlitebrowser golang nasm default-jdk terminator
   sudo apt --fix-broken install
-  #programming language support
+  snap install docker
+  
   sudo python3 -m pip install --upgrade 
   
   sudo apt install -fy wireshark python3 python3-pip sqlitebrowser golang terminator openvpn gnome-tweaks exiftool nasm binwalk default-jdk radare2 gdb gqrx-sdr clusterssh audacity bloodhound tor torbrowser-launcher nmap masscan exploitdb armitage set dsniff nikto osrframework recon-ng netdiscover legion voiphopper zaproxy enum4linux dmitry dnsrecon dnstracer theharvester thc-ipv6 reaver aircrack-ng rtlsdr-scanner gqrx-sdr wifite pixiewps burpsuite dirb dirbuster gobuster wpscan wordlists sqlmap sqlninja uniscan websploit ffuf siparmyknife powersploit backdoor-factory veil-evasion bettercap beef-xss rtpflood crunch hash-identifier john johnny rainbowcrack hashcat arduino
