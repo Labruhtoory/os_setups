5a61047eff8449302d9cdb414501c4d9e8361039
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-07-25 20:16:52 +0000

    Update setup.sh

--- pwnnotes/setup.sh
+++ pwnnotes/setup.sh
@@ -15,7 +15,8 @@ echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /home/$uboi/.bas
 echo "alias screenres='bash /opt/screenres.sh'" >> /home/$uboi/.bash_aliases
 echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> /home/$uboi/.bash_aliases
 echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> /home/$uboi/.bash_aliases
-echo "alias g='git add . && git commit -am 'changes' && git push"
+echo "alias g=lol" >> /home/$uboi/.bash_aliases 
+# "git add . && git commit -am 'changes' && git push"
 
 source /home/$uboi/.bash_aliases
 
@@ -29,8 +30,8 @@ echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /root/.bash_alia
 echo "alias screenres='bash /opt/screenres.sh'" >> /root/.bash_aliases
 echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> /root/.bash_aliases
 echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> /root/.bash_aliases
-echo "alias g='git add . && git commit -am 'changes' && git push"
-
+echo "alias g=lol" >> /home/$uboi/.bash_aliases
+# "git add . && git commit -am 'changes' && git push"
 source /root/.bash_aliases
 
 echo DONE!
