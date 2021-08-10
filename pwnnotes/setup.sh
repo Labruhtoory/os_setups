59c33e818b2388e2d98f5da5d2707307fa6c3db9
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-08-10 03:00:18 +0000

    Update setup.sh

--- pwnnotes/setup.sh
+++ pwnnotes/setup.sh
@@ -15,7 +15,8 @@ echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /home/$uboi/.bas
 echo "alias screenres='bash /opt/screenres.sh'" >> /home/$uboi/.bash_aliases
 echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> /home/$uboi/.bash_aliases
 echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> /home/$uboi/.bash_aliases
-echo "alias g=lol" >> /home/$uboi/.bash_aliases 
+echo "alias ga='git add * && git commit && git push'" >> /home/$uboi/.bash_aliases
+echo "alias gs='echo git remote set-url origin git@github.com:Username/Project.git'" >> /home/$uboi/.bash_aliases
 # "git add . && git commit -am 'changes' && git push"
 
 source /home/$uboi/.bash_aliases
@@ -30,7 +31,8 @@ echo "alias bloodhound='cd bash /opt/bloodhound/Bloodhound'" >> /root/.bash_alia
 echo "alias screenres='bash /opt/screenres.sh'" >> /root/.bash_aliases
 echo "alias thmvpn='sudo openvpn ~/Desktop/thm.ovpn'" >> /root/.bash_aliases
 echo "alias htbvpn='sudo openvpn ~/Desktop/htb.ovpn'" >> /root/.bash_aliases
-echo "alias g=lol" >> /root/.bash_aliases
+echo "alias gs='echo git remote set-url origin git@github.com:Username/Project.git'" >> /root/.bash_aliases
+echo "alias ga='git add * && git commit && git push'" >> /root/.bash_aliases
 # "git add . && git commit -am 'changes' && git push"
 source /root/.bash_aliases
 
