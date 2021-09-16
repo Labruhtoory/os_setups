49ab947d7503801b11a73e8abcf6f44e4844fcd9
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-09-15 22:05:26 -0800

    Update arch_env.sh

--- arch_env.sh
+++ arch_env.sh
@@ -40,3 +40,14 @@ Save and Exit
 ## Desktop
 sudo pacman -Rscn gnome
 pacman -S --needed xfce4 mousepad parole ristretto thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screensaver xfce4-taskmanager xfce4-wavelan-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin file-roller network-manager-applet leafpad epdfview galculator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings capitaine-cursors arc-gtk-theme xdg-user-dirs-gtk
+
+# setting resolution of display
+  touch screenres.sh
+  echo 'xrandr --newmode "1920x1080"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync' > screenres.sh
+  echo 'xrandr --addmode Virtual1 1920x1080' >> screenres.sh
+  echo 'xrandr --output Virtual1 --mode 1920x1080' >> screenres.sh
+  chmod +x screenres.sh
+  bash screenres.sh
+
+# hide desktop icons
+  xfconf-query -c xfce4-desktop -np '/desktop-icons/style' -t 'int' -s '0'
