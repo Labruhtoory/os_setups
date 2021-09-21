1f4ca51d91d32f54d0cc5816cc10a2c85d7502a8
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-09-21 04:56:01 +0000

    Update arch_tools.sh

--- arch/arch_tools.sh
+++ arch/arch_tools.sh
@@ -1,4 +1,14 @@
+# Add repo
+pacman -S curl sudo
+cd /opt
+curl -O https://blackarch.org/strap.sh
+chmod +x strap.sh
+sudo ./strap.sh
+
 #All 
+
+sudo pacman -S dmitry dnsmap dnsrecon dnstracer enum4linux masscan nikto nmap recon-ng osrframework set smbmap smtp-user-enum thc-ipv6 sslstrip ssldump theharvester wireshark-cli wireshark-qt tcpdump sqlmap sqlninja aircrack-ng gqrx wifite pyrit burpsuite zaproxy dirb dirbuster gobuster powerfuzzer pwncat metasploit backdoor-factory beef rtlsdr-scanner exploitdb binwalk bulk-extractor dex2jar dexpatcher gdb gdb-common dumpzilla pdf-parser bettercap dnschef responder siparmyknife john johnny hash-identifier hashcat wpscan wordlistctl android-sdk android-apktool jeb-android arduino radare2 javasnoop bloodhound
+
 #Anti-forensic
 #Automation 
 #Backdoor 
