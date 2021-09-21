e86611741cbb017f6328497e8c9ec4e2e7afea0f
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-09-21 05:02:08 +0000

    Update arch_tools.sh

--- arch/arch_tools.sh
+++ arch/arch_tools.sh
@@ -10,45 +10,88 @@ sudo ./strap.sh
 sudo pacman -S dmitry dnsmap dnsrecon dnstracer enum4linux masscan nikto nmap recon-ng osrframework set smbmap smtp-user-enum thc-ipv6 sslstrip ssldump theharvester wireshark-cli wireshark-qt tcpdump sqlmap sqlninja aircrack-ng gqrx wifite pyrit burpsuite zaproxy dirb dirbuster gobuster powerfuzzer pwncat metasploit backdoor-factory beef rtlsdr-scanner exploitdb binwalk bulk-extractor dex2jar dexpatcher gdb gdb-common dumpzilla pdf-parser bettercap dnschef responder siparmyknife john johnny hash-identifier hashcat wpscan wordlistctl android-sdk android-apktool jeb-android arduino radare2 javasnoop bloodhound
 
 #Anti-forensic
-#Automation 
+sudo pacman -S anti-forensic
+#Automation
+sudo pacman -S automation
 #Backdoor 
+sudo pacman -S backdoor
 #Binary 
+sudo pacman -S binary
 #Bluetooth 
-#Code-audits 
+sudo pacman -S bluetooth
+#Code-audit 
+sudo pacman -S code-audit 
 #Crackers 
+sudo pacman -S cracker
 #Crypto 
+sudo pacman -S crypto
 #Database 
+sudo pacman -S database
 #Debuggers 
+sudo pacman -S debugger
 #Decompilers 
+sudo pacman -S decompiler
 #Defensive 
-#Disassemblers 
+sudo pacman -S defensive
+#Disassemblers
+sudo pacman -S
 #Dos 
+sudo pacman -S
 #Drone 
+sudo pacman -S
 #Exploitation 
+sudo pacman -S
 #Fingerprint 
+sudo pacman -S
 #Firmware 
+sudo pacman -S
 #Forensic 
+sudo pacman -S forensic
 #Fuzzers 
+sudo pacman -S
 #Hardware 
+sudo pacman -S
 #Honeypots 
+sudo pacman -S
 #Keyloggers 
+sudo pacman -S
 #Malware 
-#Miscellaneous 
+sudo pacman -S
+#Miscellaneous
+sudo pacman -S
 #Mobile 
+sudo pacman -S 
 #Networking 
+sudo pacman -S networking
 #Nfc 
+sudo pacman -S nfc
 #Packer 
+sudo pacman -S packer
 #Proxies 
+sudo pacman -S proxies
 #Radio 
-#Reconnaissance 
+sudo pacman -S radio
+#Reconnaissance
+sudo pacman -S reconnaissance
 #Reversing 
+sudo pacman -S reversing
 #Scanners 
+sudo pacman -S scanners
 #Sniffers 
+sudo pacman -S sniffers
 #Social 
+sudo pacman -S social
 #Spoof 
+sudo pacman -S spoof
 #Stego 
+sudo pacman -S stego
 #Tunnels 
+sudo pacman -S tunnels
 #Voip 
+sudo pacman -S voip
 #Webapp 
+sudo pacman -S webapp
 #Windows 
+sudo pacman -S windows
 #Wireless
+sudo pacman -S wireless
